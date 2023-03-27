b193cc47bf50bf6474aa3d2a00b33a73c4e21e47
joacopaz <joacopaz@gmail.com>
2023-03-27 20:24:03 -0300

    Pushing repository

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Chat_GPT_desktop
+# Chat GPT desktop interface
+
+A simple Chat GPT interface running the latest available version (3.5 turbo).
+
+- To run on Linux, download the ChatGPT-linux script and execute on a terminal with `./ChatGPT-linux`.
+
+- To run on Windows simply execute the `ChatGPT.exe`.
---
+++ script.js
@@ -0,0 +1,37 @@
+const { config } = require("dotenv");
+config();
+const { Configuration, OpenAIApi } = require("openai");
+const readline = require("readline");
+
+console.log("Welcome to ChatGPT 3.5 turbo! You may exit with 'E'");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	prompt: "You: ",
+});
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const generateQuery = async (query) => {
+	const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [{ role: "user", content: query }],
+	});
+	process.stdout.clearLine();
+	readline.cursorTo(process.stdout, 0);
+	console.log(completion.data.choices[0].message.content.trim());
+};
+
+rl.prompt();
+rl.on("line", async (query) => {
+	if (query.split("")[0].toLowerCase() === "e" && query.length === 1)
+		return process.exit();
+	process.stdout.write("Loading...");
+	await generateQuery(query);
+	process.stdout.write("You: ");
+});
